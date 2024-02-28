ca43a273436766629161544171b4afe71494e317
Piyush singh <114461687+thisthat587@users.noreply.github.com>
2024-02-28 10:29:21 +0530

    .

--- rollTheDiceApp/App.tsx
+++ rollTheDiceApp/App.tsx
@@ -1,30 +1,99 @@
-import { Image, StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import React, { useState } from 'react';
+import { View, Image, Text, TouchableOpacity, StyleSheet, ImageSourcePropType } from 'react-native';
+import HapticFeedback from 'react-native-haptic-feedback';
+import type { PropsWithChildren } from 'react';
 
-const App = () => {
+import DiceOne from './assets/One.png';
+import DiceTwo from './assets/Two.png';
+import DiceThree from './assets/Three.png';
+import DiceFour from './assets/Four.png';
+import DiceFive from './assets/Five.png';
+import DiceSix from './assets/Six.png';
+
+// TODO:
+
+type DiceProps = PropsWithChildren<{
+  imageUrl: ImageSourcePropType
+}>
+
+const Dice = ({ imageUrl }: DiceProps): JSX.Element => {
   return (
-    <View style={styles.container}>
-
-      <Image
-        style={styles.image}
-        source={require('C:\\Users\\Piyush singh\\Downloads\\dice.jpg')}
-      />
-
+    <View>
+      <Image style={styles.image} source={imageUrl} />
     </View>
   )
 }
 
-export default App
+const App = () => {
+  const [imagePath, setImagePath] = useState<ImageSourcePropType>(DiceOne);
+
+  const rollTheDice = () => {
+    const randomNumber = Math.floor(Math.random() * 6) + 1;
+
+    switch (randomNumber) {
+      case 1:
+        setImagePath(DiceOne);
+        break;
+      case 2:
+        setImagePath(DiceTwo);
+        break;
+      case 3:
+        setImagePath(DiceThree);
+        break;
+      case 4:
+        setImagePath(DiceFour);
+        break;
+      case 5:
+        setImagePath(DiceFive);
+        break;
+      case 6:
+        setImagePath(DiceSix);
+        break;
+      default:
+        setImagePath(DiceOne);
+        break;
+    }
+  }
+  return (
+    <View style={styles.container}>
+      <Dice imageUrl={imagePath} />
+      {/* <Image source={imagePath} style={styles.image} /> */}
+      <View>
+        <TouchableOpacity onPress={rollTheDice}
+          style={styles.button}
+        >
+          <Text style={styles.btnTxt}>Roll the dice</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
-    alignItems: 'center'
+    alignItems: 'center',
+    backgroundColor: 'white'
   },
   image: {
     width: 200,
     height: 200,
-  }
-})
+    borderRadius: 10
 
+  },
+  button: {
+    backgroundColor: 'aliceblue',
+    padding: 16,
+    borderRadius: 5,
+    margin: 6,
+
+  },
+  btnTxt: {
+    fontSize: 16,
+    color: 'black',
+    fontWeight: 'bold',
+  }
+});
+
+export default App;
