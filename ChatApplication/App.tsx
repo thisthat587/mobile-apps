138c0337b99f21b96e8257183f9c608d4f08da2f
Piyush singh <114461687+thisthat587@users.noreply.github.com>
2024-03-03 16:18:37 +0530

    ..

--- ChatApplication/App.tsx
+++ ChatApplication/App.tsx
@@ -1,14 +1,49 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import {
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+
+import React from 'react';
 
 const App = () => {
   return (
-    <View>
-      <Text>App</Text>
-    </View>
-  )
-}
+    <ScrollView>
+      <StatusBar backgroundColor={'white'} />
+      <View style={styles.chatField}></View>
+      <View style={styles.inputBoxContainer}>
+        <TextInput style={styles.inputBox}></TextInput>
+        <TouchableOpacity>
+          <Text style={styles.arrowBtn}>⏩</Text>
+        </TouchableOpacity>
+      </View>
+    </ScrollView>
+  );
+};
 
-export default App
+export default App;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  chatField: {
+    height: 720,
+  },
+  inputBoxContainer: {
+    flex:1,
+    flexDirection: 'row',
+  },
+  inputBox: {
+    backgroundColor: 'gray',
+    width: 340,
+    borderRadius: 100,
+    marginLeft:2,
+  },
+  arrowBtn: {
+    fontSize: 32,
+    marginTop: 4,
+    marginLeft: 6,
+  },
+});
