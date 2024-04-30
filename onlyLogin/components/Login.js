51ffefa6c81f3105c76adb1eda1a7ae0efea1724
Piyush Singh <thisthat587@gmail.com>
2024-04-30 23:21:23 +0530

    login interface

---
+++ onlyLogin/components/Login.js
@@ -0,0 +1,152 @@
+import { View, Text, StyleSheet, TextInput, TouchableOpacity, ScrollView } from "react-native";
+import Svg, { Path } from "react-native-svg";
+import React from "react";
+export default function Login() {
+    return (
+        <ScrollView>
+
+            <View style={styles.loginPage}>
+                <View style={styles.upperDesign}>
+                    <Text style={styles.title}>Instagram</Text>
+                </View>
+                <View style={styles.usernameView}>
+                    <TextInput
+                        style={styles.inputBox}
+                        placeholder="Username or Email"
+                    ></TextInput>
+                    <TextInput
+                        secureTextEntry={true}
+                        style={styles.inputBox}
+                        placeholder="Password"
+                    ></TextInput>
+                    <TouchableOpacity style={styles.forgotPwdBtn}>
+                        <Text style={styles.forgotPwdBtn.text}>Forgot Password?</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={styles.loginBtn}>
+                        <Text style={styles.loginBtn.text}>Login</Text>
+                    </TouchableOpacity>
+                    <View style={styles.orContainer}>
+                        <View style={styles.orLine}></View>
+                        <Text style={styles.orText}>Or</Text>
+                        <View style={styles.orLine}></View>
+                    </View>
+                    <View style={styles.gLoginContainer}>
+                        <TouchableOpacity style={styles.gLoginButton}>
+                            <Svg height="32" width="32" viewBox="0 0 48 48">
+                                <Path fill="#EA4335" d="M24 9.5c3.54 0 6.71 1.22 9.21 3.6l6.85-6.85C35.9 2.38 30.47 0 24 0 14.62 0 6.51 5.38 2.56 13.22l7.98 6.19C12.43 13.72 17.74 9.5 24 9.5z" />
+                                <Path fill="#4285F4" d="M46.98 24.55c0-1.57-.15-3.09-.38-4.55H24v9.02h12.94c-.58 2.96-2.26 5.48-4.78 7.18l7.73 6c4.51-4.18 7.09-10.36 7.09-17.65z" />
+                                <Path fill="#FBBC05" d="M10.53 28.59c-.48-1.45-.76-2.99-.76-4.59s.27-3.14.76-4.59l-7.98-6.19C.92 16.46 0 20.12 0 24c0 3.88.92 7.54 2.56 10.78l7.97-6.19z" />
+                                <Path fill="#34A853" d="M24 48c6.48 0 11.93-2.13 15.89-5.81l-7.73-6c-2.15 1.45-4.92 2.3-8.16 2.3-6.26 0-11.57-4.22-13.47-9.91l-7.98 6.19C6.51 42.62 14.62 48 24 48z" />
+                                <Path fill="none" d="M0 0h48v48H0z" />
+                            </Svg>
+                            <Text style={styles.gLoginText}>Continue with Google</Text>
+                        </TouchableOpacity>
+                    </View>
+
+                </View>
+            </View>
+        </ScrollView>
+    )
+}
+
+const styles = StyleSheet.create({
+    loginPage: {
+        flex: 1
+    },
+    upperDesign: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: '#00aeff',
+        width: '100%',
+        height: 200,
+    },
+    title: {
+
+        fontSize: 25,
+        // marginTop: 100,
+        fontStyle: 'italic',
+        // padding: 10,
+        // textAlign:'center'
+        textDecorationLine: "underline"
+    },
+    usernameView: {
+        flex: 1,
+        // flexDirection: 'column',
+        alignItems: 'center',
+        // justifyContent: 'center',
+        width: '100%',
+        gap: 10,
+        marginTop: 80,
+    },
+    inputBox: {
+        // backgroundColor: 'aliceblue',
+        borderRadius: 6,
+        width: '85%',
+        // paddingVertical: 10,
+        padding: 7,
+        fontWeight: '600',
+        fontSize: 16,
+        borderColor: 'gray',
+        borderWidth: 0.5,
+    },
+    forgotPwdBtn: {
+        text: {
+            textAlign: 'right',
+            textDecorationLine: 'underline',
+            color: '#11aafe'
+        },
+        textAlign: 'right',
+        width: '100%',
+        // paddingHorizontal: 30
+        marginRight: 70
+    },
+    loginBtn: {
+        text: {
+            textAlign: 'center',
+            fontWeight: 'bold',
+            fontSize: 20
+        },
+        backgroundColor: '#00aeff',
+        width: '85%',
+        padding: 10,
+        borderRadius: 8,
+        marginTop: 16
+    },
+
+    orContainer: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        marginVertical: 20,
+        width: '85%'
+    },
+    orLine: {
+        flex: 1,
+        height: 1,
+        backgroundColor: 'gray'
+    },
+    orText: {
+        width: 25,
+        textAlign: 'center'
+    },
+    gLoginContainer: {
+        textAlign:'center',
+        alignItems: 'center',
+        // justifyContent: 'center',
+        width:'100%',
+    },
+    gLoginButton: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        backgroundColor: '#00aeff',
+        width:'85%',
+        padding: 10,
+        borderRadius: 8,
+    },
+    gLoginText: {
+        marginLeft: 10,
+        color: '#757575',
+        fontWeight: 'bold',
+        fontSize:16
+    },
+});
